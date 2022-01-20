3dd4e74dc72adfabeb5ae0f00e85ca26c77d736c
Fabio Dias Rhein <gft31@localiza.com>
2022-01-20 18:38:17 -0300

    added store folder

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,40 +1,31 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class Sidebar extends Component {
+import { connect } from "react-redux";
 
-    state = {
-        modules: [
-            {
-                id: 1, title: 'Iniciando com React', lessons: [
-                    { id: 1, title: 'Primeira Aula' },
-                    { id: 2, title: 'Segunda Aula' }
-                ]
-            },
-            {
-                id: 2, title: 'Aprendendo Redux', lessons: [
-                    { id: 3, title: 'Terceira Aula' },
-                    { id: 4, title: 'Quarta Aula' }
-                ]
-            }
-        ]
-    }
-
-    render() {
-        const { modules } = this.state;
-
-        return (
-            <aside>
-                {modules.map(module => (
-                    <div key={module.id}>
-                        <strong>{module.title}</strong>
-                        <ul>
-                            {module.lessons.map(lesson => (
-                                <li key={lesson.id}>{lesson.title}</li>
-                            ))}
-                        </ul>
-                    </div>
-                ))}
-            </aside>
-        )
+function toggleLesson(module, lesson) {
+    return {
+        type: 'TOGGLE_LESSON',
+        module,
+        lesson
     }
 }
+
+const Sidebar = ({ modules, dispatch }) => {
+    return (<aside>
+        {modules.map(module => (
+            <div key={module.id}>
+                <strong>{module.title}</strong>
+                <ul>
+                    {module.lessons.map(lesson => (
+                        <li key={lesson.id}>
+                            {lesson.title}
+                            <button onClick={() => dispatch(toggleLesson(module, lesson))}>Selecionar</button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        ))}
+    </aside>);
+};
+
+export default connect(state => ({ modules: state.modules }))(Sidebar);
